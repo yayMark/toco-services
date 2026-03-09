ab0f1b283f2034cdcfc937f573558c943d347f0d
Mark Hewitt <mark@clubhewitt.com>
2026-03-10 07:00:22 +0800

    First

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <img
+        className="logo"
+        src="/toco-services-logo.svg"
+        alt="Toco Services logo"
+      />
+      <div className="name">ToCo Services</div>
+      <div className="abn">ABN 56 868 552 013</div>
+    </>
+  );
+}
+
+export default App;
